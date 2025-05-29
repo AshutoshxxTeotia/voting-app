ad649ec5be4f84dda82de4e3af8e60eaf0fb2d08
Ashutosh <ashutosh10teotia@gmail.com>
2025-05-29 22:38:29 +0530

    adding voting functionality

--- votingapp/src/main/java/com/voting/votingapp/controller/PollController.java
+++ votingapp/src/main/java/com/voting/votingapp/controller/PollController.java
@@ -1,12 +1,12 @@
 package com.voting.votingapp.controller;
 
 import com.voting.votingapp.model.Poll;
+import com.voting.votingapp.request.Vote;
 import com.voting.votingapp.service.PollService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -15,9 +15,23 @@ public class PollController {
 
     private final PollService pollService;
 
-    @PostMapping()
+    @PostMapping
     public Poll createPoll(@RequestBody Poll poll){
         return pollService.createPoll(poll);
+    }
+    @GetMapping
+    public List<Poll> getAllPolls(){
+        return pollService.getAllPolls();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Poll> getPollById(@PathVariable Long id){
+        return pollService.getPollById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+    @PostMapping("/vote")
+    public void vote(@RequestBody Vote vote){
+        pollService.vote(vote.getPollId(),vote.getOptionIndex());
     }
 
 }
--- votingapp/src/main/java/com/voting/votingapp/service/PollService.java
+++ votingapp/src/main/java/com/voting/votingapp/service/PollService.java
@@ -1,9 +1,14 @@
 package com.voting.votingapp.service;
 
+import com.voting.votingapp.model.OptionVote;
 import com.voting.votingapp.model.Poll;
 import com.voting.votingapp.repository.PollRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -14,4 +19,24 @@ public class PollService {
     public Poll createPoll(Poll poll) {
         return pollRepository.save(poll);
     }
+    public List<Poll> getAllPolls() {
+     return pollRepository.findAll();
+    }
+
+    public Optional<Poll> getPollById(Long pollId) {
+        return pollRepository.findById(pollId);
+    }
+
+    public void vote(long pollId, int optionIndex) {
+        Poll poll = pollRepository.findById(pollId)
+                .orElseThrow(()->new RuntimeException("Poll not found"));
+
+            List<OptionVote> allOptions = poll.getOptions();
+            if(optionIndex < 0 || optionIndex >= allOptions.size()){
+                throw new IllegalArgumentException("Invalid option index");
+            }
+            OptionVote selectedOption = allOptions.get(optionIndex);
+            selectedOption.setVoteCount(selectedOption.getVoteCount()+1);
+            pollRepository.save(poll);
+        }
 }
